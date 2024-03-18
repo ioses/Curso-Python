700372f1920a8145cca1c7436442005fbc6e21fe
ioses <iosesgale@gmail.com>
2024-03-18 20:45:20 +0100

    Cambios 1803

--- functions.py
+++ functions.py
@@ -1,9 +1,11 @@
-def get_todos(filepath="Files/todos.txt"):
+FILEPATH = "Files/todos.txt"
+
+def get_todos(filepath=FILEPATH):
     with open(filepath, 'r') as file:
         todos = file.readlines()
     return todos
 
-def write_todos(filepath, todos):
+def write_todos(todos, filepath=FILEPATH):
     with open(filepath, 'w') as file:
         file.writelines(todos)
 
