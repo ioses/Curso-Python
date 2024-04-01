82a54698cbb427814ab1f086aaa46105dd077b7a
ioses <iosesgale@gmail.com>
2024-04-01 22:50:57 +0200

    bonus example 0104

--- bonus/bonus16.py
+++ bonus/bonus16.py
@@ -1,23 +1,28 @@
 import PySimpleGUI as sg
+from zipcreator import make_archive
 
 label1 = sg.Text("Select files to compress:")
 input1 = sg.Input()
-choose_button1 = sg.FileBrowse("Choose" key = "Files")
+choose_button1 = sg.FileBrowse("Choose", key = "Files")
 
 
 label2 = sg.Text("Select destination folder:")
 input2 = sg.Input()
-choose_button2 = sg.FolderBrowse("Choose" kewy="Folder")
+choose_button2 = sg.FolderBrowse("Choose", key="Folder")
 
 compress_button = sg.Button("Compress")
+output_label = sg.Text()
+
 window = sg.Window("File compressor", layout=[[label1, input1, choose_button1],
                                               [label2, input2, choose_button2],
-                                              [compress_button]])
+                                              [compress_button, output_label]])
 
 while True:
     event, values = window.read()
     print (event, values)
     filepaths = values["Files"].split(";")
     folder = values["Folder"]
+    make_archive(filepaths, folder)
+    window["output"].update(value="Compression completed")
 
 window.close()
---
+++ bonus/zipcreator.py
@@ -0,0 +1,14 @@
+import zipfile
+import pathlib
+
+def make_archive(filepaths, dest_dir):
+    dest_path = pathlib.Path(dest_dir, "compressed.zip")
+    with zipfile.ZipFile(dest_path, 'w') as archive: 
+        filepath = pathlib.Path(filepath)
+        for filepath in filepaths:
+            archive.write(filepath, arcname=filepath.name)
+
+
+if __name__ == "__main__":
+    make_archive(filepaths=["bonus4.py", "bonus7.py"], dest_dir="./dest")
+
