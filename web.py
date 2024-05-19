02bbfa23e79fb6b76c7aa8b3ca08df9b068ff451
ioses <iosesgale@gmail.com>
2024-05-19 22:27:35 +0200

    Cambios para añadir elementos en web

--- web.py
+++ web.py
@@ -3,6 +3,10 @@ import functions
 
 todos = functions.get_todos()
 
+def add_todo():
+    todo = st.session_state["new_todo"]+"\n"
+    todos.append(todo)
+    functions.write_todos(todos)
 
 st.title("My ToDo App")
 st.subheader("This is my ToDo App")
@@ -12,5 +16,8 @@ for todo in todos:
     st.checkbox(todo)
 
 
-st.text_input(label ="", placeholder="Add new Todo")
+st.text_input(label ="", placeholder="Add new Todo", on_change=add_todo, key='new_todo')
 
+print("Hello")
+
+st.session_state
