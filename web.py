26b1072d7593fe9890264460c529267b13017ac9
ioses <iosesgale@gmail.com>
2024-08-24 16:07:30 +0200

    añadida about

--- web.py
+++ web.py
@@ -5,6 +5,8 @@ import functions
 
 todos = functions.get_todos()
 
+st.set_page_config(layout="wide")
+
 def add_todo():
     todo = st.session_state["new_todo"]+"\n"
     todos.append(todo)
@@ -12,7 +14,9 @@ def add_todo():
 
 st.title("My ToDo App")
 st.subheader("This is my ToDo App")
-st.write("This app is to increase your productivity")
+st.write("<h1>This app is to increase your <b>productivity<b>. </h1>", unsafe_allow_html=True) ##HTML solo se permite en el metodo Write
+
+st.text_input(label ="", placeholder="Add new Todo", on_change=add_todo, key='new_todo')
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key=todo)
@@ -24,7 +28,7 @@ for index, todo in enumerate(todos):
         st.rerun()
 
 
-st.text_input(label ="", placeholder="Add new Todo", on_change=add_todo, key='new_todo')
+
 
 print("Hello")
 
