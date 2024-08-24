d7de014a43d0457c1febe6305013261df752e90b
ioses <iosesgale@gmail.com>
2024-08-24 15:28:59 +0200

    web 24082024

--- web.py
+++ web.py
@@ -1,5 +1,7 @@
-import streamlit as st
+import streamlit as st 
 import functions
+
+## Para ejecutar con streamlit: streamlit run web.py
 
 todos = functions.get_todos()
 
@@ -12,8 +14,14 @@ st.title("My ToDo App")
 st.subheader("This is my ToDo App")
 st.write("This app is to increase your productivity")
 
-for todo in todos:
-    st.checkbox(todo)
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
 
 
 st.text_input(label ="", placeholder="Add new Todo", on_change=add_todo, key='new_todo')
